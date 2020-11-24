4da5ad03ac1f15100875ee2b7e42c1b23db7fd6e
tatarize <tatarize@gmail.com>
2020-11-23 17:25:18 -0800

    1.3.0

--- svgelements/svgelements.py
+++ svgelements/svgelements.py
@@ -45,7 +45,7 @@ Though not required the SVGImage class acquires new functionality if provided wi
 and the Arc can do exact arc calculations if scipy is installed.
 """
 
-SVGELEMENTS_VERSION = "1.2.10"
+SVGELEMENTS_VERSION = "1.3.0"
 
 MIN_DEPTH = 5
 ERROR = 1e-12
