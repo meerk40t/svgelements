4ce9fed42368ddd1c5a85c1303bda8cdbe1b54dc
Tatarize <tatarize@gmail.com>
2021-03-11 02:33:02 -0800

    Version Bump, Add Bug Test

--- svgelements/svgelements.py
+++ svgelements/svgelements.py
@@ -43,7 +43,7 @@ Though not required the SVGImage class acquires new functionality if provided wi
 and the Arc can do exact arc calculations if scipy is installed.
 """
 
-SVGELEMENTS_VERSION = "1.4.8"
+SVGELEMENTS_VERSION = "1.4.9"
 
 MIN_DEPTH = 5
 ERROR = 1e-12
