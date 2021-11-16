c53aea12d720aa5253199893ead29b0b492b4080
Tatarize <tatarize@gmail.com>
2021-11-16 09:55:51 -0800

    Restore npoint/point test.

--- test/test_quadratic_bezier.py
+++ test/test_quadratic_bezier.py
@@ -20,17 +20,18 @@ class TestElementQuadraticBezierPoint(unittest.TestCase):
             self.assertTrue(np.all(np.array([list(b.start), list(b.end)])
                                    == b.npoint([0, 1])))
 
-    # def test_quadratic_bezier_point_implementations_match(self):
-    #     import numpy as np
-    #     for _ in range(1000):
-    #         b = get_random_quadratic_bezier()
-    #
-    #         pos = np.linspace(0, 1, 100)
-    #
-    #         v1 = b.npoint(pos)
-    #         # with disable_numpy():
-    #         v2 = b.npoint(pos) # Test rendered useless
-    #
-    #         for p, p1, p2 in zip(pos, v1, v2):
-    #             self.assertEqual(b.point(p), Point(p1))
-    #             self.assertEqual(Point(p1), Point(p2))
+    def test_quadratic_bezier_point_implementations_match(self):
+        import numpy as np
+        for _ in range(1000):
+            b = get_random_quadratic_bezier()
+
+            pos = np.linspace(0, 1, 100)
+
+            v1 = b.npoint(pos)
+            v2 = []
+            for i in range(len(pos)):
+                v2.append(b.point(pos[i]))
+
+            for p, p1, p2 in zip(pos, v1, v2):
+                self.assertEqual(b.point(p), Point(p1))
+                self.assertEqual(Point(p1), Point(p2))
