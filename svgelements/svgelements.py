3cdb850c18384a7ce9c2ce7d1d06b1a64821650a
Tatarize <tatarize@gmail.com>
2022-06-13 07:50:14 -0700

    version bump 1.6.17

--- svgelements/svgelements.py
+++ svgelements/svgelements.py
@@ -43,7 +43,7 @@ Though not required the Image class acquires new functionality if provided with 
 and the Arc can do exact arc calculations if scipy is installed.
 """
 
-SVGELEMENTS_VERSION = "1.6.16"
+SVGELEMENTS_VERSION = "1.6.17"
 
 MIN_DEPTH = 5
 ERROR = 1e-12
